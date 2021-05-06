8a7b53943a6ffac3d2ed99a09c48f6fe05545c57
chandan0-1 <chandanofficial.cn@gmail.com>
2021-05-06 13:43:16 +0530

    Added addQuantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
 class CartItem extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      price :999,
+      title:'Mobile Phone',
+      qty:1,
+      img:''
+    }
+    // this.addQuantity = this.addQuantity.bind(this); or use arrow function
+  }
+  
+  addQuantity = () =>{
+    console.log("this.state", this.state);
+  }
+
+
   render(){
+
+    const {price, title, qty} = this.state;
+
     return(
       <div className='cart-item'>
           {/* left block */}
@@ -11,11 +30,30 @@ class CartItem extends React.Component{
 
           {/* right block */}
           <div className="right-block">
-            <div style={ {fontSize:25} } >Phone</div>
-            <div style={ { color: '#777'} } >Rs 999</div>
-            <div style={ { color: '#777'} } >Qty: 1 </div>
+            <div style={ {fontSize:25} } >{title}</div>
+            <div style={ { color: '#777'} } >Rs: {price}</div>
+            <div style={ { color: '#777'} } >Qty: {qty} </div>
+            
             <div className="cart-item-actions">
-              {/* buttons */}
+              <img 
+                alt="add" 
+                className="action-icons" 
+                src="https://www.flaticon.com/svg/vstatic/svg/1828/1828926.svg?token=exp=1620287344~hmac=c06abb80efc58a09e2da8abd7a0ed913" 
+                onClick={this.addQuantity}
+              />
+
+              <img 
+                alt="decrease" 
+                className="action-icons" 
+                src="https://www.flaticon.com/svg/vstatic/svg/1828/1828906.svg?token=exp=1620287407~hmac=e4be64d4bab1aab23f75378694012aca" 
+              />
+              
+              <img 
+                alt="delete" 
+                className="action-icons" 
+                src="https://www.flaticon.com/svg/vstatic/svg/3389/3389017.svg?token=exp=1620287884~hmac=883a76e6b259db6d16e168eb48b7efbc" 
+              />
+
             </div>
           </div>
       </div>
